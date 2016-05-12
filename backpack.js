e2abf9cad3a5f9a942d513ad06bb11d555eacfa9
Nmuta Jones <nmuta1@gmail.com>
2016-05-12 08:46:38 -0600

    add initial files

---
+++ backpack.js
@@ -0,0 +1,8 @@
+var brand = "Sienna";
+var breakfast = "quinoa";
+var lunch = "apple";
+var weight = 5.6;
+var weight_capacity = 10;
+
+
+module.exports = weight;
---
+++ run.js
@@ -0,0 +1,4 @@
+var bag = require('./backpack');
+
+
+console.log(bag);
